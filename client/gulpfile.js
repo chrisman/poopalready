6c311144c2b5c3896e6f8722359013f872f25b52
Christopher Brown <christopher.p.brown@gmail.com>
2016-03-05 08:56:30 -0700

    init commit

---
+++ client/gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');  
+var rename = require('gulp-rename');  
+var minify = require('gulp-uglify'); 
+
+var reload = require('gulp-livereload');
+var sync = require('browser-sync').create();
+
+gulp.task('controllers', () => {
+  return gulp.src('src/js/controllers/**/*.js')
+    .pipe(concat('controllers.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('sass', () => {
+  return gulp.src('src/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(sync.stream());
+});
+
+gulp.task('html', () => {
+  return gulp.src(['src/jade/**/*.jade', '!src/jade/includes/*'])
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('watcher', () => { 
+  sync.init({
+    server: './app/',
+    open: true
+  }, function callback(){
+    gulp.watch('app/index.html', sync.reload)
+  })
+  gulp.watch('src/js/controllers/**/*.js', ['controllers']);
+  gulp.watch('src/jade/**/*.jade', ['html']);
+  gulp.watch('src/sass/**/*.sass', ['sass']);
+  reload.listen()
+
+  gulp.watch(['app/*']).on('change', reload.changed)
+});
+
+gulp.task('default', ['html', 'controllers', 'sass', 'watcher']);
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Poop Already
+
+The number two dog-walk tracker in the world.
+
+## Overview
+
+A user can create a profile, create a dog, create a new walk with that dog, and indicate whether there was a #2 or not. Walk stats are generated for the dog.
+
+User can invite other users to be co-walkers/co-owners of their dog.
+
+## Links
+
+* [front end deployment](https://numbertwo.firebaseapp.com/)
+
+* [back end deployment](https://damp-forest-20439.herokuapp.com/)
+
+* [issue tracker](https://waffle.io/chrisman/poopalready)
+
+* [github repo](https://github.com/chrisman/poopalready)
